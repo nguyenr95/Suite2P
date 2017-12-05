aa891eb35016c1d96fbcff79917b46ae1c0b014e
shinkira <shinkira3@gmail.com>
2017-12-05 10:40:04 -0500

    suite2P_to_oasis updated to use parfor loop

--- shin/suite2P_to_oasis.m
+++ shin/suite2P_to_oasis.m
@@ -36,7 +36,12 @@ function suite2P_to_oasis(mouseNum,date_num,varargin)
         movFile = fullfile(ops0.RootDirRaw,'FOV1_00001_00001.tif'); % SI2016
     end
     
+    if isempty(gcp('nocreate'))
+        parpool(4);
+    end
+    
     tstart = tic;
+    dat = [];
     for si = 1:nSlices
         if ismember(si,sliceSet)
             if nSlices==1
@@ -57,10 +62,7 @@ function suite2P_to_oasis(mouseNum,date_num,varargin)
                 Fcell = Fcell{1};
                 FcellNeu = FcellNeu{1};
             end
-            n_cell = length(length(dat.stat));
-            pick_cell = find([dat.stat.iscell]);
-                        
-            startCellInd = 1;
+            num_roi = 10;%length(length(dat.stat));
             
             fit_mode = 'prctile'; %'exp_linear';
             base_prctile = 50;
@@ -79,77 +81,108 @@ function suite2P_to_oasis(mouseNum,date_num,varargin)
             else
                 error('%s was not copied appropriately.\n\n',file_name_from);
             end
-            %%
+            %%            
             fprintf('Deconvolving F\n\n')
             tstart = tic;
-            for ci = startCellInd:size(Fcell,1)
-                if isgood(ci)
+            ROI_num = find([dat.stat(1:num_roi).iscell]);
+            parfor ri = 1:num_roi
+                if isgood(ri)
                     %%
-                    fprintf('Analyzing ROI %d (%d / %d cells)\n',ci,sum(isgood(1:ci)),sum(isgood));
+                    fprintf('Analyzing ROI %d (%d / %d cells)\n',ri,sum(isgood(1:ri)),sum(isgood));
                     fprintf('Computing dF/F ...... ')
                     if 0
                         % compute dF
-                        dF = Fcell(ci,:) - bsxfun(@times, FcellNeu(ci,:), dat.stat(ci).neuropilCoefficient); % using the method in extractSignals.m
+                        dF = Fcell(ri,:) - bsxfun(@times, FcellNeu(ri,:), dat.stat(ri).neuropilCoefficient); % using the method in extractSignals.m
                     else
                         % compute dF/F
-                        rawTrace = Fcell(ci,:);
-                        subTrace = Fcell(ci,:) - bsxfun(@times, FcellNeu(ci,:), dat.stat(ci).neuropilCoefficient); % using the method in extractSignals.m
+                        rawTrace = Fcell(ri,:);
+                        subTrace = Fcell(ri,:) - bsxfun(@times, FcellNeu(ri,:), dat.stat(ri).neuropilCoefficient); % using the method in extractSignals.m
                         rawTraceBase = getF_(rawTrace,fit_mode,ops0.imageRate,base_prctile);
                         subTraceBase = getF_(subTrace,fit_mode,ops0.imageRate,base_prctile);
                         dF = (subTrace - subTraceBase)./rawTraceBase;
                     end
                     fprintf('Done\n')
-                    %{
-                    figure(ci)
-                    plot(dF,'-b')
-                    set(gcf,'position',[200 500 1500 400])
-                    %}
+                    
+                    % figure(ri)
+                    % plot(dF,'-b')
+                    % set(gcf,'position',[200 500 1500 400])
                     
                     %%
                     fprintf('Deconvolving ...... ');
                     g = 0.95;
-                    [c, s, b, g, lam, active_set] = sc_constrained_oasisAR1(double(dF), g, [], [], [], [], []);
+                    [c, s, b, g, lam, ~] = sc_constrained_oasisAR1(double(dF), g, [], [], [], [], []);
                     % [c, s, b, g, lam, active_set] = constrained_oasisAR1(double(dF), g, [], true, [], [], []);
                     
                     % zero padding after the last spike
                     sp = zeros(1,length(dF));
                     sp(1:length(s)) = s;
                     
-                    dFsp.slice(si).cell(ci).dF = dF;
-                    dFsp.slice(si).cell(ci).c  = c';
-                    dFsp.slice(si).cell(ci).sp = sp;
-                    dFsp.slice(si).cell(ci).b  = b;
-                    dFsp.slice(si).cell(ci).g  = g;
-                    dFsp.slice(si).cell(ci).lam = lam;
-                    dFsp.slice(si).cell(ci).neuropilCoefficient = dat.stat(ci).neuropilCoefficient;
-                    telapsed = toc(tstart);
-                    fprintf('Done in %.1f sec.\n',telapsed);
+                    DF{ri} = dF;
+                    C{ri} = c';
+                    SP{ri} = sp;
+                    B{ri} = b;
+                    G{ri} = g;
+                    LAM{ri} = lam;
+                    NC{ri} = dat.stat(ri).neuropilCoefficient;
                     
-                    S = whos;
-                    mem = sum([S.bytes])*1e-6;
-                    fprintf('%.1f MB used\n\n',mem);
+                    % dFsp.slice(si).roi(ri).dF = dF;
+                    % dFsp.slice(si).roi(ri).c  = c';
+                    % dFsp.slice(si).roi(ri).sp = sp;
+                    % dFsp.slice(si).roi(ri).b  = b;
+                    % dFsp.slice(si).roi(ri).g  = g;
+                    % dFsp.slice(si).roi(ri).lam = lam;
+                    % dFsp.slice(si).roi(ri).neuropilCoefficient = dat.stat(ri).neuropilCoefficient;
+                    % telapsed = toc(tstart);
+                    % fprintf('Done in %.1f sec.\n',telapsed);
+                    
+                    %S = whos;
+                    %mem = sum([S.bytes])*1e-6;
+                    %fprintf('%.1f MB used\n\n',mem);
                 else
-                    dFsp.slice(si).cell(ci).sp = [];
+                    % dFsp.slice(si).roi(ri).sp = [];
+                    % SP{ri} = [];
                 end
-                dFsp.slice(si).cell(ci).roi = ci;
+                % dFsp.slice(si).roi(ri).roi = ri;
+                % telapsed = toc(tstart);
+                % fprintf('%d sec elapsed\n',si,round(telapsed))
             end
         end
     end
+    
+    % store the computed variables in a structure
+    for si = 1
+        for ri = 1:num_roi
+            if isgood(ri)
+                dFsp.slice(si).roi(ri).dF = DF{ri};
+                dFsp.slice(si).roi(ri).c  = C{ri};
+                dFsp.slice(si).roi(ri).sp = SP{ri};
+                dFsp.slice(si).roi(ri).b  = B{ri};
+                dFsp.slice(si).roi(ri).g  = G{ri};
+                dFsp.slice(si).roi(ri).lam= LAM{ri};
+                dFsp.slice(si).roi(ri).neuropilCoefficient = NC{ri};
+                dFsp.slice(si).roi(ri).roi = ri;
+            end
+        end
+    end
+    
+    fprintf('Saving %s ... ',file_name_to);
     save(fullfile(folder_name,file_name_to),'dFsp','-append')
+    fprintf('Done\n\n');
     
     %% save Lite file
-    ROI_num = find([dat.stat.iscell]);
     Nframes = dat.ops.Nframes;
     temp = nan(length(ROI_num),Nframes);
     
     for ci = 1:length(ROI_num)
-        temp(ci,:) = dFsp.slice.cell(ROI_num(ci)).sp;
+        temp(ci,:) = dFsp.slice.roi(ROI_num(ci)).sp;
     end
     
     clear dFsp
     dFsp = temp; % dFsp just contains spike data
     [~,name,ext] = fileparts(file_name_to);
     file_name_to_lite = [name,'_Lite',ext];
+    fprintf('Saving %s ... ',file_name_to_lite);
     save(fullfile(folder_name,file_name_to_lite),'dFsp','ROI_num');
+    fprintf('Done\n\n');
     
 end
